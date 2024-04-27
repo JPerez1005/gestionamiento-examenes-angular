f7ae286ef9638cb5d80650bc1e38bb459aa4fa36
JPerez1005 <perezbuenojuliandavid@gmail.com>
2024-04-27 06:20:15 -0500

    "cambios"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './paginas/signup/signup.component';
 import { LoginComponent } from './paginas/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { LoginComponent } from './paginas/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
